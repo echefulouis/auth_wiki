a01562903a0fc4115baf59edac49515f9231bfba
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-27 12:57:28 +0100

    profile update

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("login/", views.loginpage, name='login'),
     path('logout', views.userlogout, name='logout'),
     path('profile/', profile, name='profile'),
-    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
+    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
 
 ]
